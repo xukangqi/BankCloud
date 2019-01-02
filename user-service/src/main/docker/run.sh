b61f2d01b022866ab956686975b44010b7cafc8f
xukangqi <331705233@qq.com>
2019-01-02 11:01:34 +0800

    “添加数据库配置”

--- user-service/src/main/docker/run.sh
+++ user-service/src/main/docker/run.sh
@@ -4,4 +4,7 @@ echo "Starting the User Service"
 echo "********************************************************"
 java -Djava.security.egd=file:/dev/./urandom  -Dserver.port=$SERVER_PORT \
      -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI   \
+     -Dspring.datasource.url=$DATABASE_URI \
+     -Dspring.datasource.username=$USERNAME \
+     -Dspring.datasource.password=$PASSWORD \
      -jar /usr/local/userservice/@project.build.finalName@.jar
