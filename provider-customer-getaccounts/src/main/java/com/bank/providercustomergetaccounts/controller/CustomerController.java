07857b2f2e6e1785c56b70ef32272ccc7a58983d
xukangqi <331705233@qq.com>
2019-01-02 08:36:29 +0800

    解决冲突

--- provider-customer-getaccounts/src/main/java/com/bank/providercustomergetaccounts/controller/CustomerController.java
+++ provider-customer-getaccounts/src/main/java/com/bank/providercustomergetaccounts/controller/CustomerController.java
@@ -32,11 +32,11 @@ public class CustomerController {
 
     // 返回该id的客户所有账户信息（返回中文乱码）
     @RequestMapping(value = "/account/{custId}", method = RequestMethod.GET)
-    @ResponseBody
-    public BankResult getAccounts(@PathVariable String custId) {
+@ResponseBody
+public BankResult getAccounts(@PathVariable String custId) {
         List<BankAccount> accountList = accountService.getAccounts(custId);
         return new BankResult(200, "ok", accountList);
-    }
+        }
 
 
-}
+        }
