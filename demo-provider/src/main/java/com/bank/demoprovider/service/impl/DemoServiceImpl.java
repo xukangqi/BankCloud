9c309ffbd2fd55cce3849e02e9b7ed985fc21172
xukangqi <331705233@qq.com>
2019-01-03 19:32:17 +0800

    add random test

--- demo-provider/src/main/java/com/bank/demoprovider/service/impl/DemoServiceImpl.java
+++ demo-provider/src/main/java/com/bank/demoprovider/service/impl/DemoServiceImpl.java
@@ -6,6 +6,8 @@ import com.bank.demoprovider.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class DemoServiceImpl  implements DemoService {
 
@@ -14,9 +16,27 @@ public class DemoServiceImpl  implements DemoService {
 
     @Override
     public String hello(){
+        waitForTest();
         return "hello";
     }
 
+    public void waitForTest(){
+        Random random=new Random();
+        int result=random.nextInt(10);
+        if(result<5){
+            try{
+                Thread.sleep(100);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }else {
+            try{
+                Thread.sleep(3000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
     @Override
     public BankUser test() {
         return  bankUserMapper.selectByPrimaryKey("18805862675");
